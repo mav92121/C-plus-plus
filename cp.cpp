e2212e3ac29ffb28bae8c86938d8bf295a15551d
MAV <mohsinali530121@gmail.com>
2022-03-20 19:19:06 +0530

    standard template for cp

--- cp.cpp
+++ cp.cpp
@@ -11,49 +11,20 @@
 // #define fo(i,a,b) for(int i=a;i<=b;i++)
 #include <bits/stdc++.h>
 #define endl "\n"
+// #define int long long
 using namespace std;
 void solve()
 {
-    int n, x;
-    string s;
-    cin >> n >> x >> s;
-    vector<int> v;
-    int count = 0;
-    int ma = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (s[i] == '0')
-            count++;
-        else
-        {
-            if (count != 0)
-                v.push_back(count);
-            ma = max(ma, count);
-            count = 0;
-        }
-    }
-    ma = max(ma, count);
-    if (count != 0)
-    v.push_back(count);
-    int ans=0;
-    int tem;
-    for(int i=0;i<v.size();i++)
-    {
-        if(v[i]==ma)
-        {
-            tem=max(v[i]+v[i-1],v[i]+v[i+1]);
-            ans=max(ans,tem);
-        }
-    }
-    int temp=int (ans+1)/x;
-    cout<<temp<<endl;
+    
 }
 int main()
 {
-    int t;
+    // ios_base::sync_with_stdio(false);cin.tie(0);
+    int t = 1;
     cin >> t;
     while (t--)
     {
         solve();
     }
+    return 0;
 }
